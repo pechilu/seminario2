810ba58ea0e621da57f7fa815a6b934f4cc6dfbc
Chipi <pabmosie@upv.es>
2015-10-05 09:23:36 +0200

    Actividad 2 TAG 0.1

---
+++ actividad2.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end("Hola mundo");
+}).listen(80, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:80/');
+	
